8e6f0b33a950904c5a930a2f129a080621852c29
guiyan <guiyanwork@163.com>
2017-11-24 18:39:20 +0800

    Listview

--- StudyAndroid/app/src/main/java/com/haiersmart/studyandroid/activities/FruitActivity.java
+++ StudyAndroid/app/src/main/java/com/haiersmart/studyandroid/activities/FruitActivity.java
@@ -1,16 +1,27 @@
 package com.haiersmart.studyandroid.activities;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.haiersmart.studyandroid.R;
+import com.haiersmart.studyandroid.adapter.FruitListAdapter;
 import com.haiersmart.studyandroid.base.BaseActivity;
 
 public class FruitActivity extends BaseActivity {
+    FruitListAdapter mAdapter;
+    ListView mListview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mAdapter = new FruitListAdapter(FruitActivity.this, mListview);
+        mListview = (ListView) findViewById(R.id.fruit_list);
+        mListview.setAdapter(mAdapter);
+        mAdapter.clearItems();
+        mAdapter.addItem(R.mipmap.fruit_apple,  "Apple",  "Three apples");
+        mAdapter.addItem(R.mipmap.fruit_banana, "Banana", "Five bananas");
+        mAdapter.addItem(R.mipmap.fruit_orange, "Orange", "Two oranges");
+        mAdapter.update();
     }
 
     @Override
