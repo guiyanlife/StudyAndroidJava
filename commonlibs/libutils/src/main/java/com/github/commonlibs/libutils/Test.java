609490ce569c45d477b05867a6ea5358a2183483
guiyan <guiyanwork@163.com>
2018-07-05 11:07:19 +0800

    完成P002_hios使用的集成

---
+++ commonlibs/libutils/src/main/java/com/github/commonlibs/libutils/Test.java
@@ -0,0 +1,8 @@
+package com.github.commonlibs.libutils;
+
+/**
+ * Created by shining on 2017/9/1.
+ */
+
+public class Test {
+}
---
+++ p002_hios/src/main/java/com/github/studyandroid/hios/application/MyApplication.java
@@ -0,0 +1,31 @@
+package com.github.studyandroid.hios.application;
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
+
+import com.github.commonlibs.libwebview.hios2.HiosHelper;
+
+public class MyApplication extends MultiDexApplication {
+    public static final String DIR_PROJECT = "/hios/app/";
+    public static final String DIR_CACHE = DIR_PROJECT + "cache/";   // 网页缓存路径
+    public static final String IMG_CACHE = DIR_PROJECT + "image/";   // image缓存路径
+    public static final String VIDEO_CACHE = DIR_PROJECT + "video/"; // video缓存路径
+    public static final String MUSIC_CACHE = DIR_PROJECT + "music/"; // music缓存路径
+
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        configHios();
+    }
+
+    private void configHios() {
+        HiosHelper.config("ad.github.web.page", "github.web.page");
+    }
+
+}
