cf1ef02af819cd0d641c195a1aab4b193e09573d
guiyan <guiyanwork@163.com>
2018-09-07 15:43:43 +0800

    增加地图经纬度信息的数据库文件

--- p007_map/src/main/java/com/github/studyandroid/map/widget/WmapView.java
+++ p007_map/src/main/java/com/github/studyandroid/map/widget/WmapView.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.github.studyandroid.map.R;
